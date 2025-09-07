b91bbe8caad40aa5df97e54b5cfe30518903cc63
M ibad <128877913+Mibadd@users.noreply.github.com>
2025-09-07 14:33:25 +0700

    Revert "Auth"

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 
+// Ambil elemen dari HTML
 const loginSection = document.querySelector<HTMLDivElement>('#loginSection')!
 const welcomeSection = document.querySelector<HTMLDivElement>('#welcomeSection')!
 const loginForm = document.querySelector<HTMLFormElement>('#loginForm')!
@@ -7,16 +8,16 @@ const usernameInput = document.querySelector<HTMLInputElement>('#username')!
 const passwordInput = document.querySelector<HTMLInputElement>('#password')!
 const messageElement = document.querySelector<HTMLParagraphElement>('#message')!
 const logoutButton = document.querySelector<HTMLButtonElement>('#logoutButton')!
-const loggedInUserSpan = document.querySelector<HTMLSpanElement>('#loggedInUser')!
 
+// Event login
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault()
+
   const username = usernameInput.value
   const password = passwordInput.value
 
   if (username === 'user' && password === 'password123') {
     messageElement.textContent = ''
-    loggedInUserSpan.textContent = username
     loginSection.style.display = 'none'
     welcomeSection.style.display = 'block'
   } else {
@@ -25,6 +26,7 @@ loginForm.addEventListener('submit', (event) => {
   }
 })
 
+// Event logout
 logoutButton.addEventListener('click', () => {
   usernameInput.value = ''
   passwordInput.value = ''
